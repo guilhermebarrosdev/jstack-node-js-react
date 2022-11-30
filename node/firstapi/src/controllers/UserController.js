e06c923e62f2330df2308c77a1349d86e3718132
Guilherme Barros <wingui3@gmail.com>
2022-11-30 13:21:53 -0300

    :sparkles: Capturando body das request

--- node/firstapi/src/controllers/UserController.js
+++ node/firstapi/src/controllers/UserController.js
@@ -20,4 +20,26 @@ module.exports = {
     if (!user) return response.send(400, { error: 'User not found' });
     response.send(200, user);
   },
+
+  createUser(request, response) {
+    let body = '';
+
+    request.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    request.on('end', () => {
+      body = JSON.parse(body);
+
+      const lastUserId = users[users.length - 1].id;
+      const newUser = {
+        id: lastUserId + 1,
+        name: body.name,
+      };
+
+      users.push(newUser);
+
+      response.send(200, newUser);
+    });
+  },
 };
